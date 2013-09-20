90de7230b71f318081e4cc6138a491372ae063e5
Manin Iliy <bkmzvf@gmail.com>
2013-09-20 09:44:43 +0700

    ^...init...^

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+group :assets do
+  gem 'coffee-rails',         '~> 4.0.0'
+  gem 'compass-rails',        github: 'milgner/compass-rails', branch: 'rails4'
+  gem 'execjs'
+  gem 'jquery-rails'
+  gem 'sass-rails',           '~> 4.0.0'
+  gem 'turbolinks'
+  gem 'uglifier',             '>= 1.3.0'
+end
+
+group :default do
+  gem 'pg'
+  gem 'rails',                '4.0.0'
+end
+
+group :development do
+  gem 'hirb'
+  gem 'brakeman'
+end
+
+group :production do
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awards::Application.config.secret_key_base = '6a32e66af264716beace811deec78eb931b2b4d96fc9b069ad161cd9bef789eaff36eddda050a164de5ecd2c992173688ce5ae68c0517458773d36ab9fb4de38'
